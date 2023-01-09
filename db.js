103359a6bc6608f8d19b3c617dd03799dc568da2
Sander Sierra <sierrasander29@gmail.com>
2023-01-09 16:53:17 -0500

    Actualiza base de datos

---
+++ db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const MONGO_URL = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/inventarioelectronica"
+
+mongoose.set("strictQuery", false)
+mongoose.connect(MONGO_URL)
+
+mongoose.connection.on("connected", () => {
+    console.log("Conectado a la base de datos!")
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log(`Error al conectar a la base de datos: ${err}`)
+})
+  
+mongoose.connection.on("disconnected", () => {
+    console.log('Desconectado de la base de datos')
+})
