0ef88c80e72dd4c2ee31e8ca7790629df0ae0d62
Sander Sierra <sierrasander29@gmail.com>
2023-01-09 00:49:15 -0500

    Añade ruta de register con validacion

--- routes/site.js
+++ routes/site.js
@@ -1,17 +1,29 @@
 import { Router } from "express";
 
+import { validateRegister, validateLogin } from "../validators/users.js"
+import User from "../models/User.js"
+
+
 const router = Router()
 
 router.get("/", (req, res) => {
     res.send("Bienvenido a InventarioElectronica")
 })
 
-router.post("/login", (req, res) => {
+router.post("/login", validateLogin, async(req, res) => {
 
 })
 
-router.post("/register", (req, res) => {
+router.post("/register", validateRegister, async(req, res) => {
+    const { name, email, password } = req.body
 
+    const createdUser = new User({
+        name: name,
+        email: email,
+        password: password
+    })
+    await createdUser.encryptPassword()
+    await createdUser.save()
 })
 
 export default {router: router, endpoint: "/"}
