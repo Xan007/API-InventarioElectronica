f1bb4537a01b7bb810af5662797184c2fbe3417a
Sander Sierra <sierrasander29@gmail.com>
2023-01-09 00:21:21 -0500

    Añade nuevos paquetes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import morgan from "morgan"
+import routes from "./routes/index.js"
 dotenv.config()
+
+import "./passportJWT.js"
 
 const app = express()
 
 app.use(morgan("dev"))
 app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.send("Holaa")
-})
+app.use("/", routes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando en el puerto ${process.env.PORT}`)
