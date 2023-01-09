5a22d7505bd0a4ab93209a1487fab2c10134ef4f
Sander Sierra <sierrasander29@gmail.com>
2023-01-09 00:49:30 -0500

    Añade metodos para usuario

--- models/User.js
+++ models/User.js
@@ -1,13 +1,16 @@
 import mongoose, { Schema, model } from "mongoose"
+import bcrypt from "bcryptjs"
 
 const UserSchema =  new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
@@ -19,6 +22,14 @@ const UserSchema =  new Schema({
             ref: "Component"
         }
     ]
+})
+
+UserSchema.method("encryptPassword", async function () {
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+UserSchema.method("validatePassword", async function (plainPassword) {
+    return bcrypt.compare(plainPassword, this.password)
 })
 
 const User = model("User", UserSchema)
