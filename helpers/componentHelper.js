5545d05c7a1d168aabb9528b6cdad895be0bb819
Sander Sierra <sierrasander29@gmail.com>
2023-01-10 17:21:39 -0500

    Añadido helper para modelos

--- helpers/componentHelper.js
+++ helpers/componentHelper.js
@@ -1,28 +1,31 @@
-export const updateComponent = async(componentsArray, componentId, amount) => {
-    const index = componentsArray.findIndex((arr) => arr.componentId === componentId)
+export const updateComponent = async (componentsArray, componentId, amount) => {
+  const index = componentsArray.findIndex(
+    (arr) => arr.componentId === componentId
+  );
 
-    if (index === -1)
-        throw new Error("Couldn't find componentId")
+  if (index === -1) throw new Error("Couldn't find componentId");
 
-    let componentToUpdate = componentsArray[index]
-    componentToUpdate.amount = amount
+  let componentToUpdate = componentsArray[index];
+  componentToUpdate.amount = amount;
 
-    componentsArray[index] = componentToUpdate
+  componentsArray[index] = componentToUpdate;
 
-    return componentsArray
-}
+  return componentsArray;
+};
 
-export const deleteComponent = async(componentsArray, componentId) => {
-    componentsArray = componentsArray.filter((arr) => arr.componentId !== componentId)
+export const deleteComponent = async (componentsArray, componentId) => {
+  componentsArray = componentsArray.filter(
+    (arr) => arr.componentId !== componentId
+  );
 
-    return componentsArray
-}
+  return componentsArray;
+};
 
-export const addComponent = async(componentsArray, componentId, amount) => {
-    componentsArray.push({
-        componentId: componentId,
-        amount: amount
-    })
+export const addComponent = async (componentsArray, componentId, amount) => {
+  componentsArray.push({
+    componentId: componentId,
+    amount: amount,
+  });
 
-    return componentsArray
-}
+  return componentsArray;
+};
