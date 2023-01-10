a57468d308ed9c899d13a1b44cb2685aa6377963
Sander Sierra <sierrasander29@gmail.com>
2023-01-10 17:20:31 -0500

    Añadido validaciones extra

--- validators/projects.js
+++ validators/projects.js
@@ -1,13 +1,30 @@
-import { body } from "express-validator"
-import validateSchema from "../middleware/validateSchema.js"
+import { body, param } from "express-validator";
+import Project from "../models/Project.js";
 
-export const validateCreation = [
-    body("title")
-        .exists()
-        .notEmpty(),
-    body("about")
-        .optional(),
-    function (req, res, next) {
-        validateSchema(req, res, next)
-    }
-]
+import validateSchema from "../middleware/validateSchema.js";
+
+export const validateProject = [
+  body("title").exists().notEmpty(),
+  body("about").optional(),
+  function (req, res, next) {
+    validateSchema(req, res, next);
+  },
+];
+
+export const authProject = [
+  param("projectId").exists().isMongoId(),
+  function (req, res, next) {
+    validateSchema(req, res, next);
+  },
+  async function (req, res, next) {
+    const projectWithID = await Project.findById(req.params.projectId);
+
+    if (!projectWithID)
+      return res
+        .status(400)
+        .send(`No project with ID: ${req.params.projectId}`);
+
+    req.project = new Project(projectWithID);
+    return next();
+  },
+];
